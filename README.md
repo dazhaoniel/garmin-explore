3ae9c4b04063eba453b24e44db10e0e7d1bcb163
Daniel Zhao <danielantoiny@gmail.com>
2026-03-08 22:38:41 -0700

    add main for ease of use

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # garmin-explore
 
 Looking at some garmin .FIT data for fun
+
+
+## Development
+
+Start Jupyter Notebook
+```
+jupyter notebook
+```
+
+Run script to convert .gpx (Strava) and .fit (Garmin) activties to dataframe
+```python
+# Start python virtual environment
+source venv/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
+
+# generate requirement file
+pip freeze > requirements.txt
+
+# Leave virtual environment
+deactivate
+
+# Run script
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,26 @@
+import io
+import gzip
+import pandas as pd
+
+from pathlib import Path
+from garmin_fit_sdk import Decoder, Stream
+
+
+SOURCE_DIR = 'data'
+EXPORT_DIR = 'export'
+
+for file_path in files_in_dir:
+    file_name = str(file_path).split('/')[-1] + '.csv'
+    gz = gzip.open(file_path, 'rb').read()
+    
+    temp_file = io.BytesIO(gz)
+    
+    stream = Stream.from_byte_array(temp_file.getbuffer())
+    decoder = Decoder(stream)
+    messages, errors = decoder.read()
+    
+    # print(errors)
+    # print(messages)
+
+    df = pd.DataFrame({k: pd.Series(v) for k, v in messages.items()})
+    df.to_csv(Path(EXPORT_DIR) / file_name, index=False)
