f72ba647913ed6c111f568a6a5d1b6b0dda1ef18
Daniel Zhao <danielantoiny@gmail.com>
2026-03-08 22:41:37 -0700

    brouillon :)

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ from garmin_fit_sdk import Decoder, Stream
 SOURCE_DIR = 'data'
 EXPORT_DIR = 'export'
 
+directory_path = Path(SOURCE_DIR)
+files_in_dir = [p for p in directory_path.iterdir() if p.is_file()
+                and p.suffix == '.gz']
+
 for file_path in files_in_dir:
     file_name = str(file_path).split('/')[-1] + '.csv'
     gz = gzip.open(file_path, 'rb').read()
-    
+
     temp_file = io.BytesIO(gz)
-    
+
     stream = Stream.from_byte_array(temp_file.getbuffer())
     decoder = Decoder(stream)
     messages, errors = decoder.read()
-    
+
     # print(errors)
     # print(messages)
 
